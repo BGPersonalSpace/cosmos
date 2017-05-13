45e6414ed2785fd4f3db37145651032b01652f16
baoshan gu <baoshan@baoshans-MacBook-Pro.local>
2017-05-12 22:50:27 -0400

    added lab

--- lab/admin.py
+++ lab/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Lab, Personnel
+from .models import Lab, Personnel, LabTest, Condition, GeneAnalyte, Method, Service, Certification
 
 
 class PersonnelInline(admin.TabularInline): #(admin.StackedInline):
@@ -8,8 +8,33 @@ class PersonnelInline(admin.TabularInline): #(admin.StackedInline):
     extra = 0
 
 
+class CertificationInline(admin.TabularInline):
+    model = Certification
+    extra = 0
+
+
+class LabServiceInline(admin.TabularInline):
+    model = Lab.services.through
+    extra = 0
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    inlines = [LabServiceInline]
+
+
 class LabAdmin(admin.ModelAdmin):
     model = Lab
-    inlines = [PersonnelInline]
+    inlines = [PersonnelInline, CertificationInline, LabServiceInline]
+    exclude = ('services', )
 
+
+class TestAdmin(admin.ModelAdmin):
+    model = LabTest
+    filter_horizontal = ('conditions', 'gene_analytes', 'methods', )
+
+admin.site.register(Condition)
+admin.site.register(GeneAnalyte)
+admin.site.register(Service)
+admin.site.register(Method)
 admin.site.register(Lab, LabAdmin)
+admin.site.register(LabTest, TestAdmin)
--- lab/models.py
+++ lab/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from country.models import Country
 
 
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Lab(models.Model):
     name = models.CharField(max_length=200)
     institution = models.CharField(max_length=200, null=True, blank=True)
@@ -17,6 +24,7 @@ class Lab(models.Model):
     data_sharing = models.BooleanField()
     last_updated = models.DateTimeField(auto_now=True)
     gtr_lab_id = models.PositiveIntegerField()
+    services = models.ManyToManyField(Service)
 
     def __str__(self):
         return self.name
@@ -34,3 +42,45 @@ class Personnel(models.Model):
 
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
+
+
+class Certification(models.Model):
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    number = models.CharField(max_length=32)
+    date_expiration = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Condition(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class GeneAnalyte(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Method(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class LabTest(models.Model):
+    name = models.CharField(max_length=255)
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
+    conditions = models.ManyToManyField(Condition)
+    gene_analytes = models.ManyToManyField(GeneAnalyte)
+    methods = models.ManyToManyField(Method)
+
+    def __str__(self):
+        return self.name
